76306aff5528176e3850c3587447508a957b1967
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2020-07-22 14:07:45 -0300

    Adiciona toat personalizado para botão de alerta e erro

--- app/src/main/java/com/estudo/customtoast/MainActivity.java
+++ app/src/main/java/com/estudo/customtoast/MainActivity.java
@@ -1,6 +1,7 @@
 package com.estudo.customtoast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final int SUCCESS = 1;
+    private static final int WARNING = 0;
+    private static final int ERROR = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +26,38 @@ public class MainActivity extends AppCompatActivity {
                 showToast(SUCCESS, getString(R.string.text_success));
             }
         });
+        findViewById(R.id.button_warning).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast(WARNING, getString(R.string.text_warning));
+            }
+        });
+        findViewById(R.id.button_error).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast(ERROR, getString(R.string.text_error));
+            }
+        });
     }
 
     private void showToast(int type, String message) {
         ViewGroup view = findViewById(R.id.container_toast);
         View v = getLayoutInflater().inflate(R.layout.custom_toast, view);
+
+        switch (type) {
+            case SUCCESS:
+                v.setBackground(ContextCompat.getDrawable(this, R.drawable.toast_success));
+                break;
+            case WARNING:
+                v.setBackground(ContextCompat.getDrawable(this, R.drawable.toast_warning));
+                break;
+            case ERROR:
+                v.setBackground(ContextCompat.getDrawable(this, R.drawable.toast_error));
+                break;
+
+            default:
+                throw new IllegalStateException(getString(R.string.type_error));
+        }
 
         TextView txtMessage = v.findViewById(R.id.txt_message);
         txtMessage.setText(message);
