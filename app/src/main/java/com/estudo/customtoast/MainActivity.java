e4a2c818e95692db47aa0d32acb6af43cf29acbf
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2020-07-22 13:51:32 -0300

    Cria recurso customizado para o botão de sucesso

--- app/src/main/java/com/estudo/customtoast/MainActivity.java
+++ app/src/main/java/com/estudo/customtoast/MainActivity.java
@@ -3,12 +3,38 @@ package com.estudo.customtoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SUCCESS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_success).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast(SUCCESS, getString(R.string.text_success));
+            }
+        });
+    }
+
+    private void showToast(int type, String message) {
+        ViewGroup view = findViewById(R.id.container_toast);
+        View v = getLayoutInflater().inflate(R.layout.custom_toast, view);
+
+        TextView txtMessage = v.findViewById(R.id.txt_message);
+        txtMessage.setText(message);
+
+        Toast toast = new Toast(this);
+        toast.setView(v);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
